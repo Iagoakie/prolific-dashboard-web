cf01b709fb20f743a0fda09bab885f61b1ede5af
Iago Caldas <iagocaldas58@gmail.com>
2026-06-03 17:52:42 -0300

    \fix: revert vite base config to fix Vercel deploy"

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/prolific-dashboard-web/',
   server: {
     proxy: {
       '/api/prolific': {
